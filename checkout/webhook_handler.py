95d6e0abae57324a54c15f32205369935691f4b3
Chris Moynan <Chrismoynan@outlook.com>
2024-09-29 15:55:49 +0000

    aded email confirmation and updated webhooks handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
-
 from .models import Order, OrderLineItem
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 from products.models import Product
+from profiles.models import UserProfile
+from django.conf import settings
 
 import json
 import time
-
 import stripe
 
 class StripeWH_Handler:
@@ -13,6 +15,23 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
+    
+    def _send_confirmation_email(self, order):
+        """Send the user a confirmation email"""
+        cust_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+        
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [cust_email]
+        )                
 
     def handle_event(self, event):
         """
@@ -31,17 +50,36 @@ class StripeWH_Handler:
         bag = intent.metadata.bag
         save_info = intent.metadata.save_info
 
-        latest_charge_id = intent.latest_charge
-        stripe_charge = stripe.Charge.retrieve(latest_charge_id)
+        try:
+            charge = stripe.Charge.retrieve(intent.latest_charge)
+            billing_details = charge.billing_details
+            shipping_details = intent.shipping
+            grand_total = round(charge.amount / 100, 2)  # Fix: Retrieve charge's amount
 
-        billing_details = stripe_charge.billing_details
-        shipping_details = intent.shipping
-        grand_total = round(stripe_charge.amount / 100, 2)
+        except stripe.error.StripeError as e:
+            return HttpResponse(
+                content=f'Webhook received: {event["type"]} | ERROR: {str(e)}',
+                status=500)
 
         # Clean data in the shipping details
         for field, value in shipping_details.address.items():
             if value == "":
                 shipping_details.address[field] = None
+
+        # Update profile information if save_info was checked
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
+                profile.save()
 
         order_exists = False
         attempt = 1
@@ -66,15 +104,19 @@ class StripeWH_Handler:
             except Order.DoesNotExist:
                 attempt += 1
                 time.sleep(1)
+
         if order_exists:
+            self._send_confirmation_email(order)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
                 status=200)
+
         else:
             order = None
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
@@ -110,6 +152,7 @@ class StripeWH_Handler:
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
+        self._send_confirmation_email(order)
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
             status=200)
